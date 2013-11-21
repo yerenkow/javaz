5f766c1e86dbc51cd92b0d96189e0292ca90b74c
yerenkow <yerenkow@gmail.com>
2013-11-21 17:30:23 +0200

    New utility - RotatorPartialSender

---
+++ queues/src/main/java/org/javaz/queues/impl/RotatorPartialSender.java
@@ -0,0 +1,90 @@
+package org.javaz.queues.impl;
+
+import org.javaz.queues.iface.PartialSenderFeedI;
+import org.javaz.queues.iface.RecordsFetcherI;
+import org.javaz.queues.iface.RecordsRotatorI;
+
+/**
+ * This class implements both pre-fetching data from one source and sending it batched to other
+ * using SenderFeederI
+ */
+public class RotatorPartialSender extends SimplePartialSender
+{
+    protected int rotatorFetchSize = 1000;
+    protected int rotatorFetchDelay = 60000;
+    protected RotatorFetcher target = null;
+    protected RecordsFetcherI fetcherI;
+
+    public RotatorPartialSender(RecordsFetcherI fetcherI, PartialSenderFeedI senderFeedI) {
+        super(senderFeedI);
+        this.fetcherI = fetcherI;
+    }
+
+
+    public RecordsFetcherI getFetcherI() {
+        return fetcherI;
+    }
+
+    public void setFetcherI(RecordsFetcherI fetcherI) {
+        this.fetcherI = fetcherI;
+    }
+
+    public int getRotatorFetchSize() {
+        return rotatorFetchSize;
+    }
+
+    public void setRotatorFetchSize(int rotatorFetchSize) {
+        this.rotatorFetchSize = rotatorFetchSize;
+    }
+
+    public int getRotatorFetchDelay() {
+        return rotatorFetchDelay;
+    }
+
+    public void setRotatorFetchDelay(int rotatorFetchDelay) {
+        this.rotatorFetchDelay = rotatorFetchDelay;
+    }
+
+    public void startFetching() {
+        target = new RotatorFetcher();
+        new Thread(target).start();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        if(target != null) {
+            target.setRunning(false);
+        }
+    }
+
+    class RotatorFetcher implements Runnable
+    {
+        boolean isRunning() {
+            return running;
+        }
+
+        void setRunning(boolean running) {
+            this.running = running;
+        }
+
+        protected boolean running = true;
+        @Override
+        public void run() {
+            while(running) {
+                try {
+
+                    RecordsFetcherI fetcherI = getFetcherI();
+                    if(fetcherI != null) {
+                        RecordsRotatorI rotater = RotatorsHolder.getRotater(fetcherI);
+                        addToQueueAll(rotater.getManyElements(rotatorFetchSize));
+                    }
+
+                    Thread.sleep(rotatorFetchDelay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
