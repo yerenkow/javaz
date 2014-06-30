db788e93b743bc369c0260cb0d30b14c50af72af
yerenkow <yerenkow@gmail.com>
2014-06-30 10:28:56 +0300

    Binary Search now for longs, and performance comparison with HashSet

--- util/src/main/java/org/javaz/util/BinarySearch.java
+++ util/src/main/java/org/javaz/util/BinarySearch.java
@@ -144,7 +144,7 @@ public class BinarySearch {
 
     public static List<Integer> complexMultiSearch(final int[][][] data, final int[][] negative) {
         // this is the list we'll return
-        List<Integer> integers = new LinkedList<Integer>();
+        List<Integer> retValue = new LinkedList<Integer>();
         // this first array must be single-element array.
         final int[][] firstArrayArray = data[0];
         //this is array, on which we'll iterate.
@@ -188,9 +188,61 @@ public class BinarySearch {
             }
 
             if(!anyFailed) {
-                integers.add(val);
+                retValue.add(val);
             }
         }
-        return integers;
+        return retValue;
+    }
+
+    public static List<Long> complexMultiSearch(final long[][][] data, final long[][] negative) {
+        // this is the list we'll return
+        List<Long> retValue = new LinkedList<Long>();
+        // this first array must be single-element array.
+        final long[][] firstArrayArray = data[0];
+        //this is array, on which we'll iterate.
+        final long[] firstArray = firstArrayArray[0];
+
+        //this is indices for each array-array.
+        final int[][] indices = new int[data.length][];
+        for (int i = 0; i < data.length; i++) {
+            indices[i] = new int[data[i].length];
+        }
+        final int[] negIndices = new int[negative.length];
+
+        for (indices[0][0] = 0; indices[0][0] < firstArray.length; indices[0][0]++) {
+            // main loop based on first array.
+            // what we do is step by step checking other arrays (or sets of arrays)
+            // for same value present there.
+            boolean anyFailed = false;
+            final long val = firstArray[indices[0][0]];
+
+            //simple checks for negative
+            for (int i = 0; !anyFailed && i < negative.length; i++) {
+                while(negIndices[i] < negative[i].length && negative[i][negIndices[i]] <= val) {
+                    anyFailed = val == negative[i][negIndices[i]++];
+                }
+            }
+
+            // note, that we starting from 1, not 0!
+            for (int i = 1; !anyFailed && i < data.length; i++) {
+                // scanned variant array
+                final long[][] sva = data[i];
+                boolean anyFound = false;
+                for (int j = 0; !anyFound && j < sva.length; j++) {
+                    while(indices[i][j] < sva[j].length && sva[j][indices[i][j]] <= val) {
+                        anyFound = val == sva[j][indices[i][j]++];
+                    }
+                }
+
+                if(!anyFound) {
+                    anyFailed = true;
+                }
+            }
+
+            if(!anyFailed) {
+                retValue.add(val);
+            }
+        }
+        return retValue;
     }
 }
--- util/src/test/java/org/javaz/util/test/BinarySearchTest.java
+++ util/src/test/java/org/javaz/util/test/BinarySearchTest.java
@@ -2,6 +2,7 @@ package org.javaz.util.test;
 
 import junit.framework.Assert;
 import org.javaz.util.BinarySearch;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.*;
@@ -10,29 +11,106 @@ import java.util.*;
  */
 public class BinarySearchTest {
 
+    @Ignore
     @Test
     public void testComplexSearch() {
-        int total = 100;
+        int total = 20;
         ArrayList objs = new ArrayList();
+        ArrayList objs2 = new ArrayList();
         for(int i =0 ; i < total; i++) {
-            Object[] objects = genData(8, 4, 125000, 3, 5000);
+            Object[] objects = genDataLong(8, 3, 100000, 1, 15000);
+//            Object[] objects = genDataInt(8, 4, 125000, 3, 5000);
             objs.add(objects);
+            objs2.add(convertToHashSets(objects));
+        }
+        //warm-up
+        for(int x =0; x < 10; x++) {
+            for (Iterator iterator = objs.iterator(); iterator.hasNext(); ) {
+                Object[] objects = (Object[]) iterator.next();
+                List result = BinarySearch.complexMultiSearch((long[][][]) objects[0], (long[][]) objects[1]);
+            }
+            for (Iterator iterator = objs2.iterator(); iterator.hasNext(); ) {
+                Object[] objects = (Object[]) iterator.next();
+                List result = simpleHashSetChecks((HashSet[][]) objects[0], (HashSet) objects[1]);
+            }
         }
 
         int totalFound = 0;
         long nanoTime = System.nanoTime();
         for (Iterator iterator = objs.iterator(); iterator.hasNext(); ) {
             Object[] objects = (Object[]) iterator.next();
-            List<Integer> integers = BinarySearch.complexMultiSearch((int[][][]) objects[0], (int[][]) objects[1]);
-            totalFound += integers.size();
+            List result = BinarySearch.complexMultiSearch((long[][][]) objects[0], (long[][]) objects[1]);
+            totalFound += result.size();
         }
         long nanoTime2 = System.nanoTime();
         System.out.println("totalFound = " + totalFound);
         System.out.println("per 1 ms = " + (nanoTime2 - nanoTime)/1000000.0/total);
         System.out.println("total spent ms = " + (nanoTime2 - nanoTime)/1000000.0);
+
+        totalFound = 0;
+        nanoTime = System.nanoTime();
+        for (Iterator iterator = objs2.iterator(); iterator.hasNext(); ) {
+            Object[] objects = (Object[]) iterator.next();
+            List result = simpleHashSetChecks((HashSet[][]) objects[0], (HashSet) objects[1]);
+            totalFound += result.size();
+        }
+        nanoTime2 = System.nanoTime();
+        System.out.println("totalFound = " + totalFound);
+        System.out.println("per 1 ms = " + (nanoTime2 - nanoTime)/1000000.0/total);
+        System.out.println("total spent ms = " + (nanoTime2 - nanoTime)/1000000.0);
     }
 
-    private Object[] genData(int dims, int arrs, int items, int negs, int negsCount) {
+    private List simpleHashSetChecks(HashSet[][] data, HashSet negative) {
+        List<Long> retValue = new LinkedList<Long>();
+        HashSet first = data[0][0];
+        for (Iterator iterator = first.iterator(); iterator.hasNext(); ) {
+            Long value = (Long) iterator.next();
+            if(negative.contains(value)) {
+                continue;
+            }
+
+            boolean anyFailed = false;
+            for (int i = 1; !anyFailed && i < data.length; i++) {
+                HashSet[] hashSets = data[i];
+                boolean anyFound = false;
+                for (int j = 0; !anyFound && j < hashSets.length; j++) {
+                    anyFound = hashSets[j].contains(value);
+                }
+                if(!anyFound) {
+                    anyFailed = true;
+                }
+            }
+            if(!anyFailed) {
+                retValue.add(value);
+            }
+        }
+        return retValue;
+    }
+
+    private Object convertToHashSets(Object[] objects) {
+        long[][][] data = (long[][][]) objects[0];
+        long[][] negative = (long[][]) objects[1];
+        HashSet[][] sets = new HashSet[data.length][];
+        HashSet negset = new HashSet();
+        for(int i = 0; i < data.length; i++) {
+            sets[i] = new HashSet[data[i].length];
+            for(int j = 0; j < data[i].length; j++) {
+                sets[i][j] = new HashSet();
+                for(int k = 0; k < data[i][j].length; k++) {
+                    sets[i][j].add(data[i][j][k]);
+                }
+            }
+        }
+
+        for(int i = 0; i < negative.length; i++) {
+            for(int j = 0; j < negative[i].length; j++) {
+                negset.add(negative[i][j]);
+            }
+        }
+        return new Object[] {sets, negset} ;
+    }
+
+    private Object[] genDataInt(int dims, int arrs, int items, int negs, int negsCount) {
         int from = 1;
         int to =   3*items;
         Random random = new Random();
@@ -66,6 +144,46 @@ public class BinarySearchTest {
             Collections.sort(list);
             for (int k = 0; k < negative[i].length; k++) {
                 negative[i][k] = (Integer) list.get(k);
+            }
+        }
+
+        return new Object[]{data, negative};
+    }
+
+    private Object[] genDataLong(int dims, int arrs, int items, int negs, int negsCount) {
+        int from = 1;
+        int to =   3*items;
+        Random random = new Random();
+        long[][][] data = new long[dims][][];
+        for(int i = 0; i < data.length; i++) {
+            int size = (i ==0 ? 1 : arrs);
+            data[i] = new long[size][];
+            for(int j = 0; j < data[i].length; j++) {
+
+                HashSet set = new HashSet();
+                for(int k = 0; k < items; k++) {
+                    set.add(random.nextInt(to) + from);
+                }
+                data[i][j] = new long[set.size()];
+                ArrayList list = new ArrayList(set);
+                Collections.sort(list);
+                for (int k = 0; k < data[i][j].length; k++) {
+                    data[i][j][k] = ((Integer) list.get(k)).longValue();
+                }
+            }
+        }
+        long[][] negative = new long[negs][];
+        for(int i = 0; i < negs; i++) {
+
+            HashSet set = new HashSet();
+            for(int k = 0; k < items; k++) {
+                set.add(random.nextInt(to) + from);
+            }
+            negative[i] = new long[set.size()];
+            ArrayList list = new ArrayList(set);
+            Collections.sort(list);
+            for (int k = 0; k < negative[i].length; k++) {
+                negative[i][k] = ((Integer) list.get(k)).longValue();
             }
         }
 
